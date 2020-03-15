b884c546483e6188066d29cf1186e69eeca866d7
dhrupatel97 <55866701+dhrupatel97@users.noreply.github.com>
2020-03-15 17:59:48 -0400

    Update main.py
    Added comments.

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def character_mapping(s1: str, s2: str) -> bool:
     if l1 != l2:
         return False
     else:
+        # mapping each character of string 1 to string 2
         [maps.setdefault(i,j) for i,j in zip(lst1, lst2)]
-
+        
+        # changing string 1 to get string 2 to check for mapping characters
         s3 = ''.join(maps[c] if c in maps else c for c in s1)
 
     if s3 == s2:
